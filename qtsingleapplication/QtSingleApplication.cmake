3924e9d9af90a7f67876dc47c08192f77d3d7440
Jordan Klassen <jordan@klassen.me.uk>
2015-07-03 14:12:36 -0700

    Add cmake file

---
+++ qtsingleapplication/QtSingleApplication.cmake
@@ -0,0 +1,24 @@
+# taken from OpenCOR, under the apache 2.0 license
+# Files that make up QtSingleApplication
+
+IF(WIN32)
+    SET(PLATFORM win)
+ELSE()
+    SET(PLATFORM unix)
+ENDIF()
+
+SET(SOURCES ${SOURCES}
+    src/3rdparty/QtSingleApplication/src/qtlocalpeer.cpp
+    src/3rdparty/QtSingleApplication/src/qtlockedfile.cpp
+    src/3rdparty/QtSingleApplication/src/qtlockedfile_${PLATFORM}.cpp
+    src/3rdparty/QtSingleApplication/src/qtsingleapplication.cpp
+)
+
+SET(HEADERS_MOC ${HEADERS_MOC}
+    src/3rdparty/QtSingleApplication/src/qtlocalpeer.h
+    src/3rdparty/QtSingleApplication/src/qtsingleapplication.h
+)
+
+# Include directory
+
+INCLUDE_DIRECTORIES(src/3rdparty/QtSingleApplication/src)
